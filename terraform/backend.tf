cf7052c0f7f30dab3fd163fc44809b5310c9f5d4
Daniele Ramos <danieleramos@example.com>
2023-08-30 21:47:28 -0300

    Vamos la

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
       backend "s3" {
-    bucket         = "tf-notifier-state-1cp"
+    bucket         = "tf-notifier-state-1CPIaC "
     key            = "path/to/terraform.tfstate"
-    dynamodb_table = "tf-notifier-state-1cp"
+    dynamodb_table = "tf-notifier-state-1CPIaC"
     region         = "us-east-1"
     profile        = "fiap"
   }
