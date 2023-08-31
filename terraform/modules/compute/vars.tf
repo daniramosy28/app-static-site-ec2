560a82843796a4767df5174f89b5dc3a08c75890
Daniele Ramos <danieleramos@example.com>
2023-08-30 21:56:56 -0300

    Voltando pra quando deu certo

--- terraform/modules/compute/vars.tf
+++ terraform/modules/compute/vars.tf
@@ -26,3 +26,58 @@ variable "ec2_lt_ssh_key_name" {
     type    = string
     default = "ec2_lt_ssh_key_name"
 }
+
+variable "ec2_lb_name" {
+    type    = string
+    default = "ec2-lb-name"
+}
+
+variable "ec2_lb_tg_name" {
+    type    = string
+    default = "ec2-lb-tg-name"
+}
+
+variable "ec2_lb_tg_protocol" {
+    type    = string
+    default = "HTTP"
+}
+
+variable "ec2_lb_tg_port" {
+    type    = number
+    default = 80
+}
+
+variable "ec2_lb_listener_protocol" {
+    type    = string
+    default = "HTTP"
+}
+
+variable "ec2_lb_listener_port" {
+    type    = number
+    default = 80
+}
+
+variable "ec2_lb_listener_action_type" {
+    type    = string
+    default = "forward"
+}
+
+variable "ec2_asg_name" {
+    type    = string
+    default = "ec2-asg-name"
+}
+
+variable "ec2_asg_desired_capacity" {
+    type    = number
+    default = 8
+}
+
+variable "ec2_asg_min_size" {
+    type    = number
+    default = 4
+}
+
+variable "ec2_asg_max_size" {
+    type    = number
+    default = 16
+}
