3bae4501242fcc30ac4e157f8221d7b22e0bee2e
Daniele Ramos <danieleramos@example.com>
2023-08-30 21:50:44 -0300

    atualizando o yml2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
       backend "s3" {
-    bucket         = "tf-notifier-state-1CPIaC"
+    bucket         = "tf-notifier-state-check"
     key            = "path/to/terraform.tfstate"
-    dynamodb_table = "tf-notifier-state-1CPIaC"
+    dynamodb_table = "tf-notifier-state-check"
     region         = "us-east-1"
     profile        = "fiap"
   }
