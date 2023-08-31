ec762c771274661154ca59099dcad0af8eb6c892
Daniele Ramos <danieleramos@example.com>
2023-08-30 21:25:06 -0300

    Ajustando o provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,9 +12,9 @@ terraform {
 
 }
 
-provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = ["./.aws/config"]
-  shared_credentials_files = ["./.aws/credentials"]
-  profile                  = "fiap"
-}
+#provider "aws" {
+ # region                   = "us-east-1"
+  #shared_config_files      = ["./.aws/config"]
+  #shared_credentials_files = ["./.aws/credentials"]
+  #profile                  = "fiap"
+#}
